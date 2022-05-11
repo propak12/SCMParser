7dd13e9e3fc690caedfce98a44435f2cdb095c13
Alexandr Krasnov <67280977+propak12@users.noreply.github.com>
2022-05-11 22:03:34 +0300

    Добавьте файлы проекта.

---
+++ SteamCommunityMarketParser/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SteamCommunityMarketParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(153)))), ((int)(((byte)(5)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(153)))), ((int)(((byte)(5)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(153)))), ((int)(((byte)(5)))));
+            this.button1.Location = new System.Drawing.Point(336, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 24);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(73)))), ((int)(((byte)(101)))));
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 9F);
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox1.Size = new System.Drawing.Size(318, 23);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(24)))), ((int)(((byte)(34)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9F);
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.textBox1.Location = new System.Drawing.Point(138, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(222, 120);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(18)))), ((int)(((byte)(21)))));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(40)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(372, 173);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Steam MP Checker";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SteamCommunityMarketParser/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace SteamCommunityMarketParser
+{
+    public partial class Form1 : Form
+    {
+        List<string> items = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            if (File.Exists("Listings"))
+            {
+                items = File.ReadAllLines("Listings").ToList();
+                comboBox1.Items.AddRange(items.ToArray());
+                //comboBox1.DataSource = items;
+            }
+            else
+                File.Create("Listings");
+            ChromeOptions op = new ChromeOptions();
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.SuppressInitialDiagnosticInformation = true;
+            service.HideCommandPromptWindow = true;
+            //op.AddArgument("--headless");
+            //op.AddArgument("--silent");            
+            dr = new OpenQA.Selenium.Chrome.ChromeDriver(service,op);
+            dr.Manage().Window.Minimize();
+            
+        }
+        IWebDriver dr;
+        IWebElement element;
+        string list_info = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dr.Navigate().GoToUrl("https://steamcommunity.com/market/");                                  
+            string request="";
+            request = comboBox1.Text;
+            request = request.Replace("(", "").Replace(")","");
+            if (request != "")
+            {  
+                element = dr.FindElement(By.Id("findItemsSearchBox"));                                                                                   
+                element.SendKeys(request + OpenQA.Selenium.Keys.Enter);
+                //Thread.Sleep(500);
+                List<IWebElement> CnP = dr.FindElements(By.CssSelector(".market_listing_row_link")).ToList();
+                string result = "";
+                string url = "";
+                if (CnP.Count != 0)
+                {
+                    for (int i = 0; i < CnP.Count; i++)
+                    {
+                        element = CnP[i].FindElement(By.ClassName("market_listing_item_name"));
+                        if (element.Text==request ||request.Split(' ').ToList().All(x => Regex.IsMatch(element.Text, @x, RegexOptions.IgnoreCase)))
+                        {
+                            result += element.Text+Environment.NewLine;                           
+                            if (!items.Any(x => x == element.Text))
+                            {
+                                items.Add(element.Text);                                
+                                File.Delete("Listings");
+                                File.WriteAllLines("Listings", items);
+                                comboBox1.Items.Add(element.Text);                                
+                            }
+                            dr.Url = CnP[i].GetAttribute("href");
+                            break;
+                        }
+                    }
+                    element = dr.FindElement(By.Id("largeiteminfo_item_type"));
+                    result += element.Text + Environment.NewLine;
+                    if (CheckEl(By.ClassName("market_commodity_order_summary")) != null)
+                    {
+                        Thread.Sleep(1000);
+                        element = dr.FindElement(By.ClassName("market_commodity_order_summary"));
+                        result += element.Text + Environment.NewLine + Environment.NewLine;
+                    }
+                    else
+                    {
+                        element = dr.FindElement(By.CssSelector("span[class=\"market_listing_price market_listing_price_with_fee\"]"));
+                        result +="Начальная цена: "+ element.Text+Environment.NewLine + Environment.NewLine;
+                    }
+                    Thread.Sleep(700);
+                    element = dr.FindElement(By.Id("market_commodity_buyrequests"));
+                    result += element.Text + Environment.NewLine;
+                    element = dr.FindElement(By.ClassName("market_listing_largeimage"));
+                    pictureBox1.ImageLocation = element.FindElement(By.TagName("img")).GetAttribute("src").ToString();
+                    textBox1.Text = result;
+                    //MessageBox.Show(result);                
+                }
+            } 
+            else
+                textBox1.Text = "Введите название предмета!";             
+            
+        }
+        private IWebElement CheckEl(By by)
+        {
+            List<IWebElement> webs = dr.FindElements(by).ToList();
+            if (webs.Count != 0)
+                return dr.FindElement(by);
+            else
+                return null;
+        }
+        private IWebElement CheckEl(By by,IWebElement element)
+        {
+            List<IWebElement> webs = element.FindElements(by).ToList();
+            if (webs.Count != 0)
+                return element.FindElement(by);
+            else
+                return null;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {           
+            dr.Dispose();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
